5b299a0afd96ec265c05a559e50f675b398627da
hellen <1023483215@qq.com>
2019-04-22 00:09:35 +0800

    增加带过滤的list2map

--- java-utils-demo/src/test/java/com/hellen/utils/javautilsdemo/list2map/List2MapWithDistinct.java
+++ java-utils-demo/src/test/java/com/hellen/utils/javautilsdemo/list2map/List2MapWithDistinct.java
@@ -10,6 +10,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 @Slf4j
