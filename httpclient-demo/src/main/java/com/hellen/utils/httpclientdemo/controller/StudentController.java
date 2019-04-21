d2bbdc0878aed44466be8248976c095dfe4a335f
hellen <1023483215@qq.com>
2019-04-21 22:41:15 +0800

    list

--- httpclient-demo/src/main/java/com/hellen/utils/httpclientdemo/controller/StudentController.java
+++ httpclient-demo/src/main/java/com/hellen/utils/httpclientdemo/controller/StudentController.java
@@ -1,8 +1,12 @@
 package com.hellen.utils.httpclientdemo.controller;
 
 import com.hellen.utils.httpclientdemo.vo.StudentDTO;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author daiweihang
@@ -19,5 +23,21 @@ public class StudentController {
         dto.setName("张三");
         dto.setAge("11");
         return dto;
+    }
+    @RequestMapping("/getList")
+    public List<StudentDTO> getList(){
+        ArrayList<StudentDTO> list = new ArrayList<>();
+        StudentDTO student = new StudentDTO();
+        student.setId(11L);
+        student.setAge("11");
+        student.setName("张三");
+        list.add(student);
+
+        student = new StudentDTO();
+        student.setId(12L);
+        student.setAge("12");
+        student.setName("李四");
+        list.add(student);
+        return list;
     }
 }
