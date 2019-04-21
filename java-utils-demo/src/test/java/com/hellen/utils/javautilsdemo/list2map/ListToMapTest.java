21c374549e963440f1ed08e2c18e0bc229f9afb7
hellen <1023483215@qq.com>
2019-04-21 17:30:53 +0800

    增加java.util的base64，stream的用法

--- java-utils-demo/src/test/java/com/hellen/utils/javautilsdemo/list2map/ListToMapTest.java
+++ java-utils-demo/src/test/java/com/hellen/utils/javautilsdemo/list2map/ListToMapTest.java
@@ -1,15 +1,18 @@
 package com.hellen.utils.javautilsdemo.list2map;
 
+import com.alibaba.fastjson.JSON;
 import com.hellen.utils.javautilsdemo.JavaUtilsDemoApplicationTests;
 import com.hellen.utils.javautilsdemo.vo.Student;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONString;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Slf4j
 public class ListToMapTest extends JavaUtilsDemoApplicationTests {
@@ -41,7 +44,27 @@ public class ListToMapTest extends JavaUtilsDemoApplicationTests {
         student.setName("王五");
         student.setAge(13);
         students.add(student);
+
+        student = new Student();
+        student.setName("王五");
+        student.setAge(14);
+        students.add(student);
         return students;
+    }
+
+    /**
+     * list转为map，
+     * key为某个参数，value为list内的对象本身
+     */
+    @Test
+    public void tetsList2MapSelf(){
+        List<Student> studentList = assemble();
+        List<Student> collect = studentList.stream().distinct().sorted(Comparator.comparing(Student::getAge)).collect(Collectors.toList());
+        System.out.println(collect);
+        List<Integer> integers = Arrays.asList(1,1, 3, 5, 6, 7, 8, 9);
+        List<Integer> collect1 = integers.stream().sorted().collect(Collectors.toList());
+        System.out.println(collect1);
+
     }
 
 }
